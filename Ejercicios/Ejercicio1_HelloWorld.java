f47b03830b0dfa7d3b0f83612252710d5b664f4b
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-05 12:44:24 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio1_HelloWorld.java
@@ -0,0 +1,10 @@
+/* 159949 Luis Ivan Valdez Anchondo 05/02/2019 12:25p.m */
+
+public class Ejercicio1{
+
+	public static void main(String[] args){
+
+	System.out.println("Hello! \nIvan Valdez");
+	
+	}
+}
