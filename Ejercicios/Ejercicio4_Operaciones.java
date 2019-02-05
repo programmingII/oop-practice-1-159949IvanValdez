8ff2f24428a7ddd6974a4ceedddd0c0fd820020a
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-05 13:06:39 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio4_Operaciones.java
@@ -0,0 +1,10 @@
+/* 159949 Luis Ivan Valdez Anchondo 05/02/2019 01:00 pm.*/
+
+class Ejercicio4_Operaciones{
+	public static void main(String[] args){
+	System.out.println(-5+8*6);
+	System.out.println((55+9)%9);
+	System.out.println(20+ -3*5/8);
+	System.out.println(5+15/3*2-8%3);
+	}
+}
