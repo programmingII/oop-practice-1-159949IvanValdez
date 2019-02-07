244d77199d09107eaa9785b603051b3146846da0
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-07 12:46:50 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio9_Expresion.java
@@ -0,0 +1,8 @@
+/* 159949 Luis Ivan Valdez Anchondo 07/02/2019 12:41p.m */
+
+public class Ejercicio9_Expresion{
+	public static void main(String[] args){
+	System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) );// Imprime el resultado de la operacion
+	
+	}
+}
