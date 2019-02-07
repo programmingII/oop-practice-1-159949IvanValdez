3077bbccf31ca9d061e41b8db3e4f8ca033b2341
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-07 12:38:53 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio8_Patron.java
@@ -0,0 +1,10 @@
+/* 159949 Luis Ivan Valdez Anchondo 07/02/2019 12:35p.m */
+
+public class Ejercicio8_Patron{
+	public static void main(String[] args){
+	System.out.println("   J    a   v     v  a");// imprime el mensaje en la consola
+	System.out.println("   J   a a   v   v  a a ");// imprime el mensaje en la consola
+	System.out.println("J  J  aaaaa   V V  aaaaa");// imprime el mensaje en la consola
+	System.out.println(" JJ  a     a   V  a     a");// imprime el mensaje en la consola
+	}
+}
