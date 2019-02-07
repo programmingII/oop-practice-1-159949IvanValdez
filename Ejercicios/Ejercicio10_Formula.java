ba650bda3439d7eecc6a72d8845432ab7c328587
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-07 12:55:54 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio10_Formula.java
@@ -0,0 +1,9 @@
+/* 159949 Luis Ivan Valdez Anchondo 07/02/2019 12:48p.m */
+
+public class Ejercicio10_Formula{
+	public static void main(String[] args){
+	double res=4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)); //declaro el tipo y nombre de la variable y la igualo a la formula formula
+	System.out.println(res);// imprimo el resultado de la operacion 
+
+	}
+}
