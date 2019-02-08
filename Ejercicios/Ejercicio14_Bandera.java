a9e752ea982fa3206e5f3b190173973f7544390f
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-08 14:37:01 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio14_Bandera.java
@@ -0,0 +1,23 @@
+/* 159949 Luis Ivan Valdez Anchondo 08/02/2019 02:32p.m.*/
+
+public class Ejercicio14_Bandera{
+	public static void main(String[] args){
+
+	System.out.println("* * * * * * ==================================");//aqui imprimimos en pantalla la bandera
+	System.out.println(" * * * * *  ==================================");
+	System.out.println("* * * * * * ==================================");
+	System.out.println(" * * * * *  ==================================");
+	System.out.println("* * * * * * ==================================");
+	System.out.println(" * * * * *  ==================================");
+	System.out.println("* * * * * * ==================================");
+	System.out.println(" * * * * *  ==================================");
+	System.out.println("* * * * * * ==================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+
+	}
+}
