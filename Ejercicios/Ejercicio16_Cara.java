2f85b79ff6a2155b433ddfa4df1d25847e0e329a
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-08 15:07:28 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio16_Cara.java
@@ -0,0 +1,13 @@
+/* 159949 Luis Ivan Valdez Anchondo 08/02/2019 03:03p.m.*/
+
+public class Ejercicio16_Cara{
+	public static void main(String[] args){
+	
+	System.out.println(" +'''''+ ");// lo usamos para mostrar el mensaje en la consola
+	System.out.println("[| o o |] ");
+	System.out.println(" |  ^  | ");
+	System.out.println(" | '-' | ");
+	System.out.println(" +-----+ ");
+		
+	}
+}
