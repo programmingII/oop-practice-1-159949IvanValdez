b65e0e7ee0528abfa6aab4aa6d4431f14cab19ef
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-24 12:43:29 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio43_StringFormato.java
@@ -0,0 +1,7 @@
+/* 159949 Luis Ivan Valdez Anchondo 24/02/2019  12:40pm.*/
+
+public class Ejercicio43_StringFormato {
+	public static void main(String[] args) {
+	System.out.println("\nTwinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!\n\n");//Escribe el mensaje con saltos de linea(\n) y tabulaciones (\t)
+	}
+}
