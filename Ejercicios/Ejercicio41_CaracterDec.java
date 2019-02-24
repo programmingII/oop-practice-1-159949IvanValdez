d0c7d9e9d3acc5b46107307322d0122f246f7ce1
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-24 12:51:14 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio41_CaracterDec.java
@@ -0,0 +1,10 @@
+/* 159949 Luis Ivan Valdez Anchondo 24/02/2019 12:32p.m */
+
+import java.util.Scanner; //me permite escanear una entrada por teclado
+
+public class Ejercicio41_CaracterDec{
+	public static void main(String[] args){
+		int n='Z';//declaramos la variable n como int y le asignamos Z
+		System.out.println("El valor ASCII de Z es: "+n);// al imprimir el valor de n convierte lo que contiene a su equivalente en ASCII
+	}					
+}
