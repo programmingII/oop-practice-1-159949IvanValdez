fa38d402138cfd9f4b030793d77bd25c95d8a556
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-25 13:56:06 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio46_Fecha.java
@@ -0,0 +1,7 @@
+/* 159949 Luis Ivan Valdez Anchondo 25/02/2019  01:46pm.*/
+
+public class Ejercicio46_Fecha{
+	public static void main(String[] args) {
+		System.out.format("\nLa hora actual es: %tc%n\n", System.currentTimeMillis());// con currentTimeMillis() obtenemos la hora del sistema en milisegundos 
+	}
+}
