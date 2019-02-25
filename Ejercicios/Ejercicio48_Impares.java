e605ec9eefca55c844f86271d0ec95c4ebe5137a
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-25 14:24:53 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio48_Impares.java
@@ -0,0 +1,9 @@
+/* 159949 Luis Ivan Valdez Anchondo 25/02/2019  02:23pm.*/
+
+public class Ejercicio48_Impares{
+	public static void main(String[] args){
+		for (int i = 1; i < 100; i++){//este cliclo for es para reccorer los numeros del 1 al 99
+			if (i % 2 != 0) System.out.println(i);// con esta condicion me aseguro de que solo se impriman los que son impares
+		}
+	}
+}
