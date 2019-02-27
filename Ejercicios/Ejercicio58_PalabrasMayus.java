4c4dfaeb874a0a6cbb84b8f7930df7ab2434178a
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-02-27 14:40:08 -0700

    Update Ejercicio58_PalabrasMayus.java

--- Ejercicios/Ejercicio58_PalabrasMayus.java
+++ Ejercicios/Ejercicio58_PalabrasMayus.java
@@ -13,9 +13,12 @@ public class Ejercicio58_PalabrasMayus{
 	Scanner enunScan = new Scanner(enun); // aqui creamos un objeto de la clase Scanner llamado enunScan y le asignamos lo que se capturo en la variable enun
         
 	while(enunScan.hasNext()){// usamosun while para recorrer todas las palabras del enunciado
-             String palabra = enunScan.next(); // creamos un objeto de la clase Scanner
-             Mayus += Character.toUpperCase(palabra.charAt(0)) + palabra.substring(1) + " "; 
+             String palabra = enunScan.next(); // declaramos una variable string llamada palabra que va a contener lo que tiene enunScan y con el .next() encuentra y retorna el siguiente token completo del scanner enunScan
+             Mayus += Character.toUpperCase(palabra.charAt(0)) + palabra.substring(1) + " ";
+		//aqui a la variable Mayus se le van a asignar las palabras o tokens que se leen con la variable palabra, de la clase character usamos el metodo .toUpperCase que convierte en mayusculas lo que le indicamos,
+		//en este caso lo combinamos con .charAt(0) para indicarle que solo el caracter en la posicion 0 de es estring es lo que se va a convertir en mayusculas, despues le agregamos con .substring(1) los demas
+		//de la palabra comenzando de la posicion 1 para formar la palabra completa otra vez e imprimimos " " para separar las palabras con espacios
         }
-      	System.out.println(Mayus.trim()); 
+      	System.out.println(Mayus.trim()); // imprimimos el enunciado completo y con .trim() nos aseguramos de que cualquier espacio en blando antes o despues del string sea removido
     }
 }
